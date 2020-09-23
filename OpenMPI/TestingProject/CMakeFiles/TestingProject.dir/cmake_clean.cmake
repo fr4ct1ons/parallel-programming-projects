7251c83821e6a76bd54efdf2482ad72a0381e6ee
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2020-09-23 16:14:10 -0300

    Enabled MPI

---
+++ OpenMPI/TestingProject/CMakeFiles/TestingProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestingProject.dir/main.cpp.o"
+  "TestingProject.pdb"
+  "TestingProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestingProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- OpenMPI/TestingProject/main.cpp
+++ OpenMPI/TestingProject/main.cpp
@@ -1,7 +1,18 @@
-﻿#include<iostream>
+﻿#include<stdio.h>
+#include"mpi.h"
 
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+    printf("Hello, world!\n");
+
+    int my_rank, comm_sz;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    printf("Processor %d of %d says hello!\n", my_rank, comm_sz);
+
+    MPI_Finalize();
+    return 0;
 }
