2801eb3db0bcf7e5233a22e2188b4a179cae827a
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2020-10-10 22:04:00 -0300

    Fixed float precision

--- OpenMPI/TestingProject/main (2).c
+++ OpenMPI/TestingProject/main (2).c
@@ -1,32 +1,30 @@
-﻿#include<iostream>
+﻿#include<stdio.h>
 #include"mpi.h"
-#include<string>
-#include<cmath>
-#include<chrono>
-#include<cstdio>
+#include<math.h>
+#include<time.h>
 
-long double Function(long double x)
+float Function(float x)
 {
-    //return 2.0f;
-    return sin(x) + 2.5f;
+    return 2.0f;
+    //return sin(x) + 2.5f;
 }
 
-long double TrapezeArea(long double bottomBase, long double topBase, long double height)
+float TrapezeArea(float bottomBase, float topBase, float height)
 {
     return ((topBase + bottomBase) * height)/2;
 }
 
-long double TrapezeAreaPerProcess(long double bottomBase, long double topBase, int n)
+float TrapezeAreaPerProcess(float bottomBase, float topBase, int n)
 {
-    long double totalArea = (Function(bottomBase) + Function(topBase))/2.0f;
+    float totalArea = (Function(bottomBase) + Function(topBase))/2.0f;
     
-    //long double a = 1.0f; //Function start
-    //long double b = 2500000000.0f; //Function end
-    //long double n = 2000000000; // Number of trapezes (resolution)
+    //float a = 1.0f; //Function start
+    //float b = 2500000000.0f; //Function end
+    //float n = 2000000000; // Number of trapezes (resolution)
 
-    long double h = (topBase - bottomBase) / n; // Height of the original trapeze
+    float h = (topBase - bottomBase) / n; // Height of the original trapeze
 
-    long double x_i = 0.0f;
+    float x_i = 0.0f;
 
     for (int i = 1; i < n; i++)
     {
@@ -48,36 +46,35 @@ long double TrapezeAreaPerProcess(long double bottomBase, long double topBase, i
     return totalArea;
 }
 
-//long double b = 2500000000.0f;//Function end -- Serial worst case.
+//float b = 2500000000.0f;//Function end -- Serial worst case.
 //int n = 1680000000;// Number of trapezes (resolution) -- Serial worst case.
 
-//long double a = 0.0f; //Function start -- Square.
-//long double b = 20.0f; //Function end
-//long double n = 20; // Number of trapezes (resolution)
+//float a = 0.0f; //Function start -- Square.
+//float b = 20.0f; //Function end
+//float n = 20; // Number of trapezes (resolution)
 
 int main()
 {
-    std::cout << std::fixed;
-    bool isParallel = false;
+    int isParallel = 0;
 
-    long double a = 0.0f; //Function start
-    long double b = 200000000.0f; //Function end
-    int n =   2000000000; // Number of trapezes (resolution)
+    float a = 0.0f; //Function start
+    float b = 20000000000.0f; //Function end
+    int n =   20; // Number of trapezes (resolution)
                
-    long double h = (b - a); // Height of the original trapeze
+    float h = (b - a); // Height of the original trapeze
 
-    long double x_i = 0.0f;
+    float x_i = 0.0f;
 
-    long double totalArea = (Function(a) + Function(b))/ 2.0f;
+    float totalArea = (Function(a) + Function(b))/ 2.0f;
 
     int my_rank, comm_sz;
     char message[500];
 
-    std::chrono::system_clock::time_point startTime;
+    clock_t startTime;
 
     if (isParallel)
     {
-        startTime = std::chrono::system_clock::now();
+        startTime = clock();
 
         MPI_Init(NULL, NULL);
         MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
@@ -87,15 +84,15 @@ int main()
 
         /*if (my_rank == comm_sz - 1)
         {
-            long double divRemain = fmod((long double)n, comm_sz);
+            float divRemain = fmod((float)n, comm_sz);
             std::cout << "Division remain: " << divRemain << "\n";
             localN += (int)divRemain;
         }*/
 
-        long double localH = h / (long double)comm_sz; //Height of each process' trapeze
-        long double individualH = (h/n);
+        float localH = h / (float)comm_sz; //Height of each process' trapeze
+        float individualH = (h/n);
         
-        long double localA = a + my_rank * localN * h;
+        float localA = a + my_rank * localN * h;
 
         /*
         std::cout << "------" << "\n";
@@ -111,8 +108,9 @@ int main()
         std::cout << "IndividualH: " << individualH << "\n";
         //*/
 
-        long double localIntegral = 0.0f;
-        long double x_i = 0.0f;
+        float localIntegral = 0.0f;
+        float bottomBase = 0.0f, topBase = 0.0f;
+        float x_i = 0.0f;
         for (int i = 0; i < localN; i++)
         {
             x_i = localA + i * individualH;
@@ -126,28 +124,28 @@ int main()
             std::cout << localIntegral / 2 << "\n";*/ //std::cout << localIntegral << "\n";
         }
 
-        //std::cout << "LocalIntegral: " << localIntegral << "\n";
-        //std::cout << "------" << std::endl;
+        printf("LocalIntegral: %f \n", localIntegral);
+        printf("------ \n");
 
         if (my_rank != 0) // Is not master
         {
-            MPI_Send(&localIntegral, 1, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&localIntegral, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
         }
         else // Is master
         {
-            long double totalIntegral = localIntegral;
-            long double newIntegral = 0.0f;
+            float totalIntegral = localIntegral;
+            float newIntegral = 0.0f;
             for (int proc = 1; proc < comm_sz; proc++)
             {
-                MPI_Recv(&newIntegral, 1, MPI_LONG_DOUBLE, proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(&newIntegral, 1, MPI_FLOAT, proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-                //std::cout << "New integral for process " << proc << " is: " << newIntegral << std::endl;
+                std::cout << "New integral for process " << proc << " is: " << newIntegral << std::endl;
                 totalIntegral += newIntegral;
             }
 
             auto endTime = std::chrono::system_clock::now();
 
-            std::chrono::duration<long double> processDuration = endTime - startTime;
+            std::chrono::duration<float> processDuration = endTime - startTime;
 
             std::cout << "Process duration: " << processDuration.count() << "\n";
 
@@ -160,22 +158,22 @@ int main()
     {
         startTime = std::chrono::system_clock::now();
 
-        long double localIntegral = 0.0f;
-        long double x_i = 0.0f;
-        long double individualH = (h / n);
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
-            x_i = a + i * individualH;
-            localIntegral += Function(x_i + individualH / 2) * individualH;
+            x_i = a + i * h;
+            totalArea += Function(x_i);
         }
+        //*/
+
+        totalArea *= h;
 
         auto endTime = std::chrono::system_clock::now();
 
-        std::chrono::duration<long double> processDuration = endTime - startTime;
+        std::chrono::duration<float> processDuration = endTime - startTime;
 
-        std::cout << "Process duration: " << processDuration.count() << "\n";
+        std::cout << "Process duration: " << std::fixed << processDuration.count() << "\n";
 
-        std::cout << "Area: " << std::fixed << localIntegral << std::endl;
+        std::cout << "Area: " << totalArea << std::endl;
     }
 
     
